92f30b43f211e5e038bdddb7b74aac619a0b812d
Dancing Coconut <dancingcoconut12@gmail.com>
2024-03-23 11:13:02 +0530

    IR Screenshot functionalit added, saved as screenshot_alt_2.png

--- scrape_alt.py
+++ scrape_alt.py
@@ -28,6 +28,19 @@ while True:
         urllib.request.urlretrieve(src, "screenshot_alt.png")
     except Exception as e:
         print("Done")
+
+    driver.get("https://tropic.ssec.wisc.edu/real-time/imagemain.php?&basin=indian&prod=irn&sat=m5")
+
+    time.sleep(5)
+
+    image_element = driver.find_element(By.XPATH, '/html/body/table/tbody/tr/td/center/p/img')
+    src = image_element.get_attribute('src')
+    try:
+        print("Trying")
+        urllib.request.urlretrieve(src, "screenshot_alt_2.png")
+    except Exception as e:
+        print("Done")
+    
     driver.quit()
     print("Sleeping")
     time.sleep(30 * 60)
